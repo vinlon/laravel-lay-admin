dab645dbcbe4df6032ab0e33ff2524e8f7c53977
liwenlong <vinlon1009@gmail.com>
2021-05-22 12:00:20 +0800

    update layui from v2.6.4 to v2.6.7

--- src/views/index.blade.php
+++ src/views/index.blade.php
@@ -7,7 +7,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
     <meta name="viewport"
           content="width=device-width, initial-scale=1.0, minimum-scale=1.0, maximum-scale=1.0, user-scalable=0">
-    <link rel="stylesheet" href="assets/layui-v2.6.4/css/layui.css" media="all">
+    <link rel="stylesheet" href="assets/layui-v2.6.7/css/layui.css" media="all">
 </head>
 <body>
 <div id="LAY_app"></div>
